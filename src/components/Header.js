b39a3ea569aa08a0794ae99a2dd7d9fa3cd8a4f8
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-11-18 15:12:07 +0200

    still the help page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../scss/header.scss';
 
 const Header = () => {
   return (
     <header>
       <h1>RESTy</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact to='/'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to='/history'>
+              History
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to='/help'>
+              Help
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
--- src/components/history.js
+++ src/components/history.js
@@ -4,12 +4,12 @@ import List from './list';
 function History({ req, handler }) {
   if (req.length) {
     return (
-      <div id='method'>
+      <div className='method'>
         <List data={req} handler={handler} />
       </div>
     );
   } else {
-    return <div id='method'></div>;
+    return <div className='method'></div>;
   }
 }
 
